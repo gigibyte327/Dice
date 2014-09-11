4abb2f45d489733e81c0273efd73044857c2f759
gigibyte327 <gigi.lu27@gmail.com>
2014-09-11 10:08:31 -0700

    make dice

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,13 +1,29 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
 int totRoll;
 int rd=0;
 int allRd=0;
-void setup()
+public void setup()
 {
 	size(700,700);
 	noLoop();
 	
 }
-void draw()
+public void draw()
 {
 	background(0);
 	rd=rd+1;
@@ -40,7 +56,7 @@ void draw()
 	text("Round vs Points", 450,650);*/
 }
 	
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -57,12 +73,12 @@ class Die //models one single dice cube
 		myY=y;
 		siz=30;
 	}
-	void roll()
+	public void roll()
 	{
 		//your code here
 		numDots=(int)(Math.random()*6)+1;
 	}
-	void show()
+	public void show()
 	{
 		noStroke();
 		fill(255);
@@ -109,4 +125,13 @@ class Die //models one single dice cube
 		}
 
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
